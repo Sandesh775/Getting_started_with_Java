9dcde78da42c20fd5c864f176b793c83eb6f1a63
sandesh <sandeshchapagain99@gmail.com>
2025-12-08 17:20:55 +0545

    switch case and ternary operator

---
+++ Everyday exercises/Revised.java
@@ -0,0 +1,34 @@
+// revising the concept of switch and ternary operator
+import java.util.Scanner;
+public class Revised{
+    public static void main(String [] args){
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter your first name: ");
+        String name = obj.nextLine();
+        char ch = name.charAt(0);  // Correct way to access first character , alternative : char ch = Character.toLowerCase(name.charAt(0));
+        System.out.println("Now enter your age : ");
+        int age = obj.nextInt();
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')){
+            switch (Character.toLowerCase(ch)){
+            case 'a':/**  This parts works like OR operator,execute the same block of code  */
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println("Your first name start with vowel letter ! : "+ch);
+                break;
+            default :
+                System.out.println("Your first name start with consonant letter !: "+ch);
+            } 
+        }
+        else{
+            System.out.println("Invalid input !!!! Name must start with A-Z or a-z.");
+        }
+        System.out.println("Voting eligibility : " + 
+                ((age>=18)
+                ?"You are eligible for voting !"
+                :"You are not eligible for voting !")
+                          );
+        
+    }
+}
