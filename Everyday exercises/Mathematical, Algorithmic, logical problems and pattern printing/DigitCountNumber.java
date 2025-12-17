b4ad8d70b38358184bc5809ae15bebe487646708
sandesh <sandeshchapagain99@gmail.com>
2025-12-17 10:39:11 +0545

    count digit in number

---
+++ Everyday exercises/Mathematical, Algorithmic, logical problems and pattern printing/DigitCountNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class DigitCountNumber{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any number : ");
+        int number = sc.nextInt();
+        int count = 0;
+        int digit = 0;
+        int temp = number;
+        while( temp > 0){
+            count ++;
+            temp = temp/10;
+        }
+        System.out.println("Digit count in number : "+count);
+        System.out.println("Extracting number from Right to left : ");
+        for(int i = 1; i <= count; i++){
+            digit = number % 10;
+            System.out.print(digit+"\t");
+            number = number / 10;
+        }
+    }
+}
