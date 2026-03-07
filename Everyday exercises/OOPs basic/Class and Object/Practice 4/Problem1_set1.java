20d6872cc9cd8953ad7ab36b23b96f7cddb87c72
sandesh <sandeshchapagain99@gmail.com>
2026-03-07 22:24:00 +0545

    Problem1-set1 and set2

--- Everyday exercises/OOPs basic/Class and Object/Practice 4/Problem1_set1.java
+++ Everyday exercises/OOPs basic/Class and Object/Practice 4/Problem1_set1.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 /*
 * Question 1: Student Grade Management System
 Create a Student class with:
@@ -12,18 +13,20 @@ Create two students:
 1.	Original student: "John", roll: 101, marks: [85, 90, 78]
 2.	Copy of John (using copy constructor) but with different name "John Clone"
 */
-public class Problem1_set1 {
+public class Problem1_set1{
     public static void main(String[] args) {
-        Student2 s1 = new Student2("Danish",1,new double[]{67,79,99.0});
-        //double per = s1.calculatePercentage();
+        double[] marks = {85, 90, 78};
+        Student2 s1 = new Student2("John", 101, marks);
+        System.out.println("Displaying the info of original student !");
         s1.displayInfo();
-        //System.out.println(+per);
-
+        Student2 s2 = new Student2(s1);
+        System.out.println("Displaying the info of John clone !");
+        s2.displayInfo();
     }
 }
 class Student2{
     private String name;
-    private  int rollNumber;
+    private int rollNumber;
     private double[] marks = new double[3];
     Student2(String name, int rollNumber, double[] marks){
         this.name = name;
@@ -38,43 +41,93 @@ class Student2{
     public String getName(){
         return name;
     }
-
-    public String getMarks() {
-            return "Marks 1: "+marks[0]+" Marks 2: "+this.marks[1]+" Marks 3: "+marks[2];
-    }
-
-    public void setMarks(double m1, double m2, double m3){
-        if(m1 > 0 && m1 < 100){
-            this.marks[0] = m1;
+    public void setMarks(int index, double mark){
+        if(mark >= 0 && mark <= 100){
+            marks[index] = mark;
         }
         else{
-            System.out.println("Invalid input");
-        }
-        if(m2 > 0 && m2 < 100){
-            this.marks[1] = m2;
-        }
-        else{
-            System.out.println("Invalid input");
-        }
-        if(m3 > 0 && m3 < 100){
-            this.marks[2] = m3;
-        }
-        else{
-            System.out.println("Invalid input");
+            System.out.println("Invalid marks : validation (0-100) !!");
+            return;
         }
     }
     public double calculatePercentage(){
         double sum = 0;
-        for( double d : marks){
-            sum += d;
+        double total = marks.length*100.0;
+        for( double m : marks){
+            sum += m;
         }
-        return sum/300.0*100.0;
+        return sum/total*100.0;
     }
     public void displayInfo(){
-        double per = this.calculatePercentage();
         System.out.println("Name : "+this.name+"\n"+
                 "Roll no : "+this.rollNumber+"\n"+
-                "Marks : "+this.getMarks()+"\n"+
-                "Percentage : "+per);
+                "Marks : "+ Arrays.toString(marks)+"\n"+
+                "Percentage : "+calculatePercentage());
     }
 }
+//public class Problem1_set1 {
+//    public static void main(String[] args) {
+//        Student2 s1 = new Student2("Danish",1,new double[]{67,79,99.0});
+//        //double per = s1.calculatePercentage();
+//        s1.displayInfo();
+//        //System.out.println(+per);
+//
+//    }
+//}
+//class Student2{
+//    private String name;
+//    private  int rollNumber;
+//    private double[] marks = new double[3];
+//    Student2(String name, int rollNumber, double[] marks){
+//        this.name = name;
+//        this.rollNumber = rollNumber;
+//        this.marks = marks;
+//    }
+//    Student2(Student2 obj){
+//        this.name = obj.name;
+//        this.rollNumber = obj.rollNumber;
+//        this.marks = obj.marks;
+//    }
+//    public String getName(){
+//        return name;
+//    }
+//
+//    public String getMarks() {
+//            return "Marks 1: "+marks[0]+" Marks 2: "+this.marks[1]+" Marks 3: "+marks[2];
+//    }
+//
+//    public void setMarks(double m1, double m2, double m3){
+//        if(m1 > 0 && m1 < 100){
+//            this.marks[0] = m1;
+//        }
+//        else{
+//            System.out.println("Invalid input");
+//        }
+//        if(m2 > 0 && m2 < 100){
+//            this.marks[1] = m2;
+//        }
+//        else{
+//            System.out.println("Invalid input");
+//        }
+//        if(m3 > 0 && m3 < 100){
+//            this.marks[2] = m3;
+//        }
+//        else{
+//            System.out.println("Invalid input");
+//        }
+//    }
+//    public double calculatePercentage(){
+//        double sum = 0;
+//        for( double d : marks){
+//            sum += d;
+//        }
+//        return sum/300.0*100.0;
+//    }
+//    public void displayInfo(){
+//        double per = this.calculatePercentage();
+//        System.out.println("Name : "+this.name+"\n"+
+//                "Roll no : "+this.rollNumber+"\n"+
+//                "Marks : "+this.getMarks()+"\n"+
+//                "Percentage : "+per);
+//    }
+//}
--- Everyday exercises/OOPs basic/Class and Object/Practice 4/Problem1_set2.java
+++ Everyday exercises/OOPs basic/Class and Object/Practice 4/Problem1_set2.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
 * Problem 1: Student Grade System (OOP + Arrays + Logic)..another set
 Create a Student class with:
@@ -13,8 +15,7 @@ In main:
 - Count how many students got grade 'A'
 - Display all students sorted by average (descending)
 */
-import java.util.*;
-public class Problem1_set2 {
+public class Problem1_set2{
     public static void main(String[] args) {
         Student s1 = new Student("Ram", 1, new double[]{91.0, 72, 63, 44, 55});// Create a new object and pass an anonymous array
         Student s2 = new Student("Ram1", 10, new double[]{92.0, 72, 63, 4, 15});
@@ -23,110 +24,207 @@ public class Problem1_set2 {
         Student s5 = new Student("Ram4", 10000, new double[]{91.0, 72, 69, 44, 90});
         // array of Students
         Student[] students = {s1, s2, s3, s4, s5};
-        double highest_avg = 0;
+        double highest_avg = students[0].calculateAverage();
         int index = 0;
-        for (int i = 0; i < students.length; i++) {
-            if (highest_avg < students[i].avg) {
-                highest_avg = students[i].avg;
+        for (int i = 1; i < students.length; i++) {
+            double avg = students[i].calculateAverage();
+            if (highest_avg < avg) {
+                highest_avg = avg;
                 index = i;
             }
         }
         System.out.println("Student with highest average : " + highest_avg);
         students[index].displayReport();
         int count = 0;
-        for (Student s : students) {
-            if (s.grade == "A") {
+        for( int i = 0; i<students.length; i++){
+            char grade = students[i].findGrade();
+            if(grade == 'A'){
                 count++;
             }
         }
         System.out.println(count + " students got grade 'A'");
-        for (int i = 0; i < students.length - 1; i++) {
-            for( int j = 0; j<i; j++){
-                if(students[i].avg < students[j].avg){
-                    double temp = students[i].avg;
-                    students[i].avg = students[j].avg;
-                    students[j].avg = temp;
+        // Sort students by average (descending)
+        for(int i = 0; i < students.length - 1; i++){
+            for(int j = i + 1; j < students.length; j++){
+                // Compare averages
+                if(students[i].calculateAverage() < students[j].calculateAverage()){
+                    // Swap STUDENT OBJECTS, not just averages
+                    Student temp = students[i];
+                    students[i] = students[j];
+                    students[j] = temp;
                 }
             }
         }
-        System.out.println("Display all students sorted by average (descending)");
-        for( int i = 0; i <students.length; i++){
-            for( int j = i+1; j<students.length; j++){
-                if(students[i].avg < students[j].avg){
-                    double temp = students[j].avg;
-                    students[j].avg = students[i].avg;
-                    students[i].avg = temp;
-                }
-            }
-            students[i].displayReport();
+
+// NOW print after sorting is complete
+        System.out.println("\nStudents sorted by average (descending):");
+        for(int i = 0; i < students.length; i++){
+            System.out.println((i+1) + ". " + students[i].getName() +
+                    " - Average: " + students[i].calculateAverage());
         }
     }
 }
 class Student{
-    String name;
-    int rollNumber;
-    double[] marks = new double[5];
-    double avg;
-    String grade;
-    public Student(String name, int rollNumber, double[] marks) {
+    private String name;
+    private int rollNumber;
+    private double[] marks = new double[5];
+    Student(String name, int rollNumber, double[]marks){
         this.name = name;
         this.rollNumber = rollNumber;
         this.marks = marks;
-        this.avg = this.calculateAverage(this.marks);
-        this.grade = this.findGrade(this.marks);
     }
-
-    public void displayReport(){
-        System.out.println("Name : "+this.name+"\tRoll no : "+this.rollNumber);
-        for(double d : marks){
-            System.out.println("Sub : "+d);
-        }
-        System.out.println("Average : "+this.calculateAverage(marks)+"\tGrade Obtained : "+this.findGrade(marks));
+    public String getName(){
+        return name;
     }
-    public double calculateAverage(double[] a){
-        double avg;
-        double sum = 0;
-        for(double d : a){
+    public double calculateAverage(){
+        double total , sum = 0;
+        for( double d : marks){
             sum += d;
         }
-        avg = sum/5;
-        return avg;
+        total = marks.length;
+        return sum/total;
     }
-    public String findGrade(double[] a){
-        double sum = 0;
-        double avg ;
-        for(double d : a){
-            sum += d;
-        }
-        avg = sum/5.0;
+    public char findGrade(){
+        double avg = this.calculateAverage();
         if(avg >= 90){
-            return "A+";
-        } else if (avg >= 85) {
-            return "A";
+            return 'A';
         }
         else if (avg >= 80) {
-            return "A-";
-        }
-        else if (avg >= 75) {
-            return "B+";
+            return 'B';
         }
         else if (avg >= 70) {
-            return "B";
-        }
-        else if (avg >= 65) {
-            return "B-";
+            return 'C';
         }
         else if (avg >= 60) {
-            return "C+";
-        }
-        else if (avg >= 55) {
-            return "C";
+            return 'D';
         }
         else if (avg >= 50) {
-            return "C-";
+            return 'E';
         }
         else{
-            return "NG";
+            return 'N';
         }
     }
+    public void displayReport(){
+        System.out.println("Name : "+name+"\n"+
+                "Roll no : "+rollNumber+"\n"+
+                "Marks : "+ Arrays.toString(marks)+"\n"+
+                "Average : "+calculateAverage());
+    }
+    public void displayAverage(){
+        System.out.println(this.calculateAverage());
+    }
 }
+//import java.util.*;
+//public class Problem1_set2 {
+//    public static void main(String[] args) {
+//        Student s1 = new Student("Ram", 1, new double[]{91.0, 72, 63, 44, 55});// Create a new object and pass an anonymous array
+//        Student s2 = new Student("Ram1", 10, new double[]{92.0, 72, 63, 4, 15});
+//        Student s3 = new Student("Ram2", 100, new double[]{91.0, 72, 89, 44, 55});
+//        Student s4 = new Student("Ram3", 1000, new double[]{67, 72, 6, 44, 5});
+//        Student s5 = new Student("Ram4", 10000, new double[]{91.0, 72, 69, 44, 90});
+//        // array of Students
+//        Student[] students = {s1, s2, s3, s4, s5};
+//        double highest_avg = 0;
+//        int index = 0;
+//        for (int i = 0; i < students.length; i++) {
+//            if (highest_avg < students[i].avg) {
+//                highest_avg = students[i].avg;
+//                index = i;
+//            }
+//        }
+//        System.out.println("Student with highest average : " + students[index].avg);
+//        students[index].displayReport();
+//        int count = 0;
+//        for (Student s : students) {
+//            if (s.grade == "A") {
+//                count++;
+//            }
+//        }
+//        System.out.println(count + " students got grade 'A'");
+//
+//        System.out.println("Display all students sorted by average (descending)");
+//        for( int i = 0; i <students.length - 1; i++){
+//            for( int j = i+1; j<students.length; j++){
+//                if(students[i].avg < students[j].avg){
+//                    double temp = students[j].avg;
+//                    students[j].avg = students[i].avg;
+//                    students[i].avg = temp;
+//                }
+//            }
+//            students[i].displayReport();
+//        }
+//    }
+//}
+//class Student{
+//    String name;
+//    int rollNumber;
+//    double[] marks = new double[5];
+//    double avg;
+//    String grade;
+//    Scanner sc = new Scanner(System.in);
+//    public Student(String name, int rollNumber, double[] marks) {
+//        this.name = name;
+//        this.rollNumber = rollNumber;
+//        this.marks = marks;
+//        this.avg = this.calculateAverage(this.marks);
+//        this.grade = this.findGrade(this.marks);
+//    }
+//
+//    public void displayReport(){
+//        System.out.println("Name : "+this.name+"\tRoll no : "+this.rollNumber);
+//        for(double d : marks){
+//            System.out.println("Sub : "+d);
+//        }
+//        System.out.println("Average : "+this.calculateAverage(marks)+"\tGrade Obtained : "+this.findGrade(marks));
+//    }
+//    public double calculateAverage(double[] a){
+//        double avg;
+//        double sum = 0;
+//        for(double d : a){
+//            sum += d;
+//        }
+//        avg = sum/5;
+//        return avg;
+//    }
+//    public String findGrade(double[] a){
+//        double sum = 0;
+//        double avg ;
+//        for(double d : a){
+//            sum += d;
+//        }
+//        avg = sum/5.0;
+//        if(avg >= 90){
+//            return "A+";
+//        } else if (avg >= 85) {
+//            return "A";
+//        }
+//        else if (avg >= 80) {
+//            return "A-";
+//        }
+//        else if (avg >= 75) {
+//            return "B+";
+//        }
+//        else if (avg >= 70) {
+//            return "B";
+//        }
+//        else if (avg >= 65) {
+//            return "B-";
+//        }
+//        else if (avg >= 60) {
+//            return "C+";
+//        }
+//        else if (avg >= 55) {
+//            return "C";
+//        }
+//        else if (avg >= 50) {
+//            return "C-";
+//        }
+//        else{
+//            return "NG";
+//        }
+//    }
+//    public void displayAverage(){
+//        System.out.println(this.avg);
+//    }
+//}
